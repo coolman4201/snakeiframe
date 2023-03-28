91de3f8c93ab140f1c06253507c9b33c6d59f27b
coolman4201 <129195976+coolman4201@users.noreply.github.com>
2023-03-28 10:47:37 -0400

    Create snake.js
    prolly wont work, doing this for fun

---
+++ snake.js
@@ -0,0 +1,51 @@
+var SHEEP_2048;
+(function() {
+  'use strict';
+  if (snakesheep) {
+    snakesheep();
+  } else {
+    function applyStyles(elem,obj) {
+      for (var prop in obj) elem.style[prop]=obj[prop];
+    }
+    var wrapper=document.createElement("div"),
+    frame=document.createElement("iframe");
+    frame.src="javascript:Q=64;m=b=Q*Q;a=[P=l=u=d=p=S=w=0];u=89;f=(h=j=t=(b+Q)/2)-1;(B=(D=document).body).appendChild(x=D.createElement("p"));(X=x.style).position="fixed";X.left=X.top=0;X.background="#FFF";x.innerHTML="<p></p><canvas>";v=(s=x.childNodes)[0];(s=s[1]).width=s.height=5*Q;c=s.getContext("2d"); onkeydown=onblur=F=function(e,g){g?a[f]?(w+=m,f=Math.random(l+=8)*(R=Q-2)*R|(u=0),F(f+=Q+1+2*(f/R|0),g)):F(f):0>e?(l?--l:(y=t,t=a[t]-2,F(y)),S+=(w*=0.8)/4,m=999/(u++ +10),a[h+=[-1,-Q,1,Q][d=p]]?B.removeChild(x,alert("Game Over")):(F(h),F(e,j=h),v.innerHTML=P?(setTimeout(F,50,e,0),S|0):"Press P")):-e?(y=(a[e]=e<Q|e>=Q*Q-Q|!(e%Q)|e%Q==Q-1|2*(e==h))+(e==f),e==h&&(a[j]=2+h),c.fillStyle="hsl("+99*!a[e]+","+2*m+"%,"+50*y+"%)",c.fillRect(e%Q*5,5*(e/Q|0),5,5)):isNaN(y=e.keyCode-37)|43==y?(P=y&&!P)&&F(-1): p=!P|y&-4|!(y^2^d)?p:y;return!1};for(;--b;F(b));void F(-1);";
+    frame.width=333; // additional 30px to hide scrollbars
+    frame.height=461;
+    frame.scrolling="no";
+    applyStyles(frame,{
+      position:'absolute',
+      top:'-151.5px',
+      left:'-27px',
+      border:"none"
+    });
+    applyStyles(wrapper,{
+      position:"fixed",
+      zIndex:2147483647,
+      display:"block",
+      bottom:"10px",
+      left:"10px",
+      width:"280px",
+      height:"280px",
+      overflow:"hidden",
+      transition:"opacity 0.1s",
+      borderRadius:"6px",
+      transform:"scale(1)"
+    });
+    wrapper.appendChild(frame);
+    document.body.parentNode.appendChild(wrapper);
+    function enter() {wrapper.style.opacity="1";}
+    function leave() {wrapper.style.opacity="0";frame.blur();}
+    wrapper.addEventListener("mouseenter",enter,false);
+    wrapper.addEventListener("mouseleave",leave,false);
+    snakesheep=()=>{
+      wrapper.removeEventListener("mouseenter",enter,false);
+      wrapper.removeEventListener("mouseleave",leave,false);
+      wrapper.parentNode.removeChild(wrapper);
+      frame=null;
+      wrapper=null;
+      snakesheep=undefined;
+      for (var script of document.querySelectorAll('script[src="https://sheeptester.github.io/javascripts/2048.js"]')) script.parentNode.removeChild(script);
+    };
+  }
+}());
